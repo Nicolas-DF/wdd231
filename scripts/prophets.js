072cd1d340d660a1e93a677f17405ce51604279d
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-11 09:14:50 -0300

    Finished the getProphetData function

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -2,10 +2,11 @@ const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.
 
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
+async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
 getProphetData();
