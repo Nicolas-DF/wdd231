5e1710e7093993a5e657da6ddcb94313df7cc0e9
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-15 09:40:18 -0300

    Added .toLocaleString after lastModified

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,5 +1,5 @@
-const currentYear = new Date().getFullYear;
+const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 const lastModified = new Date(document.lastModified);
-document.getElementById("lastModified").textContent = lastModified;
+document.getElementById("lastModified").textContent = lastModified.toLocaleString();
