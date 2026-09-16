1b7e3de9095bb1ea3ed96c39ca22b45975b29628
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-16 10:46:37 -0300

    Defined required values and set the api url

--- week03/weather-api/scripts/weather.js
+++ week03/weather-api/scripts/weather.js
@@ -2,3 +2,10 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
+//CREATE REQUIRED VALUES FOR THE URL
+const myKey = '1b8cd4861185b3a20cee66d9b3a7c4ed';
+const lat = 49.750596876979124;
+const lon = 6.635751475539449;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${myKey}`;
