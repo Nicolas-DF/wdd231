cfe940e1d69d7af18c4d9dc6440d147027b144d4
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-16 09:55:46 -0300

    Imported object and functions from the other files

--- week03/scripts/modules.mjs
+++ week03/scripts/modules.mjs
@@ -1,4 +1,6 @@
-
+import byuiCourse from "./course.mjs";
+import { setSectionSelection } from "./section.mjs";
+import { setTitle, renderSections } from "./output.mjs";
 
 document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
