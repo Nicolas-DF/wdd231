460ce16a41fc0371d81763c481ba8c1a787b2ac1
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-16 09:52:46 -0300

    Changed file format form .js to .mjs

---
+++ week03/scripts/modules.mjs
@@ -0,0 +1,14 @@
+
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum);
+});
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum, false);
+});
+
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
