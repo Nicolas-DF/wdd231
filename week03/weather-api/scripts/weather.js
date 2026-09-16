2d1f161169f103d28b32f7c8cc82c6c443c53569
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-16 10:52:44 -0300

    Declared the async function

--- week03/weather-api/scripts/weather.js
+++ week03/weather-api/scripts/weather.js
@@ -5,7 +5,26 @@ const captionDesc = document.querySelector('figcaption');
 
 //CREATE REQUIRED VALUES FOR THE URL
 const myKey = '1b8cd4861185b3a20cee66d9b3a7c4ed';
-const lat = 49.750596876979124;
-const lon = 6.635751475539449;
+const lat = 49.75;
+const lon = 6.64;
 
+// API URL
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${myKey}`;
+
+
+// ASYNC FUNCTION
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
