257b0e3a7a90e562e246ce78b0cd16ffc186327f
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-18 10:30:08 -0300

    Got the code for displaying weather and changed values according the right place

---
+++ chamber/scripts/temperature.js
@@ -0,0 +1,40 @@
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+//CREATE REQUIRED VALUES FOR THE URL
+const myKey = '1f1af9dfc7d0c39f461b21b79cb4156e';
+const lat = -22.00916431403047;
+const lon = -47.89173007812205;
+
+// API URL
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${myKey}`;
+
+
+// ASYNC FUNCTION
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// DISPLAY FUNCTION
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+};
+
+apiFetch();
