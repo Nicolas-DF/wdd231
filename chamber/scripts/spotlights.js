60859c5e3abf289640b18b565ebf3427b9d736c2
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-09-21 08:39:27 -0300

    Filtered the gold and silver members, mixed the order and displayed randomly

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -4,7 +4,12 @@ async function getMembers() {
     const response = await fetch(membersURL);
     const data = await response.json();
 
-    console.log(data);
+    const qualifiedMembers = data.filter(member => member.membershipLevel === 2 || member.membershipLevel === 3);
+    qualifiedMembers.sort(() => Math.random() - 0.5);
+
+    const selectedMembers = qualifiedMembers.slice(0, 3);
+
+    console.log(selectedMembers);
 }
 
 getMembers();
